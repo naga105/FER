3f9d1ef63819c065711a5a953094c4999a6952cb
naga115 <huy1152002@gmail.com>
2022-09-27 22:26:20 +0700

    lab4

--- script.js
+++ script.js
@@ -16,50 +16,67 @@ function handleSubmit() {
     console.log("submited");
 }
 
-function carousel() {
 
-    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-
-        $("#mycarousel").carousel('pause');
-
-        $("#carouselButton").children("span").removeClass('fa-pause');
-
-        $("#carouselButton").children("span").addClass('fa-play');
-
-    } else if ($("#carouselButton").children("span").hasClass('fa-play')) {
-
-        $("#mycarousel").carousel('cycle');
-
-        $("#carouselButton").children("span").removeClass('fa-play');
-
-        $("#carouselButton").children("span").addClass('fa-pause');
-
-    }
-
-};
 
 $(document).ready(function() {
+    //tooltip
+    $('[data-toggle="tooltip"]').tooltip();
 
     $("#mycarousel").carousel({
         interval: 2000
     });
+    //pause button
 
     $("#carousel-pause").click(function() {
 
         $("#mycarousel").carousel('pause');
 
     });
-
+    //play button
     $("#carousel-play").click(function() {
 
         $("#mycarousel").carousel('cycle');
 
     });
 
-});
+    // include 2
+    $("#carouselButton").click(function() {
 
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+
+            $("#mycarousel").carousel('pause');
+
+            $("#carouselButton").children("span").removeClass('fa-pause');
+
+            $("#carouselButton").children("span").addClass('fa-play');
+
+        } else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+
+            $("#mycarousel").carousel('cycle');
+
+            $("#carouselButton").children("span").removeClass('fa-play');
+
+            $("#carouselButton").children("span").addClass('fa-pause');
+
+        }
+
+    });
+
+})
 $(document).ready(function() {
 
-    $('[data-toggle="tooltip"]').tooltip();
-
+    $('#activeForm').on('click', function() {
+        $('#formModal').modal()
+    });
+    $('#formModal').on('hidden.bs.modal', function(e) {})
+    $(".dismiss").on('click', function() {
+        $('#formModal').modal('hide')
+    });
+    $('#loginForm').on('click', function() {
+        $('#loginModal').modal()
+    });
+    $('#loginModal').on('hidden.bs.modal', function(e) {})
+    $(".dismiss").on('click', function() {
+        $('#loginModal').modal('hide')
+    });
 });
